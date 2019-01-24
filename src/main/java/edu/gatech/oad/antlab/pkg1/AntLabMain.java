0c927a754d688fd186ad90c5e8891e7b35db2646
Marcelo G. Costa <33551496+Marto37@users.noreply.github.com>
2019-01-24 03:45:01 -0500

    Update AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -69,7 +69,7 @@ import edu.gatech.oad.antlab.pkg2.*;
   		  toPrint += p3.toString("gburdell3");
        //Person4 replace P4 with your name
        //and gburdell4 with your gt id
-       Person4 p4 = new Person4("P4");
+       Person4 p4 = new Person4("Marcelo");
        toPrint += p4.toString("mcosta33");
         //Person5 replace P4 with your name
         //and gburdell5 with your gt id
